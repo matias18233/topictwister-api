80072615058ea885b58b938e3ddb951c4cbcc970
Matías Cruz <mcruz@ultimamillasa.com.ar>
2021-05-03 11:10:22 -0300

    Modificaciones sobre el controlador de 'usuario'

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -7,21 +7,32 @@
 
 module.exports = {
 
+    // Vía POST
     async agregar(req, res) {
 
     },
+    
+    // Vía POST
     async editar(req, res) {
 
     },
-    async delete(req, res) {
+    
+    // Vía GET
+    async borrar(req, res) {
 
     },
+    
+    // Vía POST
     async login(req, res) {
 
     },
+    
+    // Vía GET
     async obtener(req, res) {
 
     },
+    
+    // Vía GET
     async obtenerListado(req, res) {
 
     }
