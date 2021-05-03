0b78af55e7e574b6d7104bd5e0173b335cf88eb7
Matías Cruz <mcruz@ultimamillasa.com.ar>
2021-05-03 10:18:19 -0300

    Usuarios: modelo y controlador

---
+++ api/controllers/UsuarioController.js
@@ -0,0 +1,28 @@
+/**
+ * UsuarioController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    async agregar(req, res) {
+
+    },
+    async editar(req, res) {
+
+    },
+    async delete(req, res) {
+
+    },
+    async login(req, res) {
+
+    },
+    async obtener(req, res) {
+
+    },
+    async obtenerListado(req, res) {
+
+    }
+};
---
+++ api/models/Usuario.js
@@ -0,0 +1,37 @@
+/**
+ * User.js
+ *
+ * A user who can log in to this application.
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    nombre : {
+      type : 'string',
+      required : true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+      description: '',
+      protect: true,
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    // n/a
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    // n/a
+
+  },
+};
