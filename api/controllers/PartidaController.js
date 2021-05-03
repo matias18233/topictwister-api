59c423cc08f7a316abca4a3ab70b5b4da79c9800
Matías Cruz <mcruz@ultimamillasa.com.ar>
2021-05-03 11:10:36 -0300

    Partida: modelo y controlador

---
+++ api/controllers/PartidaController.js
@@ -0,0 +1,30 @@
+/**
+ * UsuarioController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    // Vía POST
+    async agregar(req, res) {
+
+    },
+    
+    // Vía POST
+    async editar(req, res) {
+
+    },
+    
+    // Vía GET
+    async borrar(req, res) {
+
+    },
+    
+    // Vía GET
+    async obtener(req, res) {
+
+    },
+
+};
